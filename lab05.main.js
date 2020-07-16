f02d9c970946b91408c9494df480c31988702a69
bpawletz <bpawletz@gmail.com>
2020-07-16 21:17:44 +0000

    Lab 6 20200716

--- lab05.main.js
+++ lab05.main.js
@@ -94,48 +94,9 @@ class ServiceNowAdapter extends EventEmitter {
      *   that handles the response.
      */
     healthcheck(callback) {
-        this.getRecord((result, error) => {
-            /**
-             * For this lab, complete the if else conditional
-             * statements that check if an error exists
-             * or the instance was hibernating. You must write
-             * the blocks for each branch.
-             */
-            if (error) {
-                /**
-                 * Write this block.
-                 * If an error was returned, we need to emit OFFLINE.
-                 * Log the returned error using IAP's global log object
-                 * at an error severity. In the log message, record
-                 * this.id so an administrator will know which ServiceNow
-                 * adapter instance wrote the log message in case more
-                 * than one instance is configured.
-                 * If an optional IAP callback function was passed to
-                 * healthcheck(), execute it passing the error seen as an argument
-                 * for the callback's errorMessage parameter.
-                 */
-
-                this.emitOffline();
-                log.error('ServiceNow: Instance is offline. | Reason: ${error} | Instance id: ${this.id}');
-                return error;
-
-
-            } else {
-                /**
-                 * Write this block.
-                 * If no runtime problems were detected, emit ONLINE.
-                 * Log an appropriate message using IAP's global log object
-                 * at a debug severity.
-                 * If an optional IAP callback function was passed to
-                 * healthcheck(), execute it passing this function's result
-                 * parameter as an argument for the callback function's
-                 * responseData parameter.
-                 */
-                 this.emitOnline();
-                 return result;
-
-            }
-        });
+        // We will build this method in a later lab. For now, it will emulate
+        // a healthy integration by emmitting ONLINE.
+        this.emitOnline();
     }
 
     /**
@@ -147,7 +108,7 @@ class ServiceNowAdapter extends EventEmitter {
      */
     emitOffline() {
         this.emitStatus('OFFLINE');
-        log.warn('ServiceNow: Instance is unavailable. | Instance id: ${this.id}');
+        log.warn('ServiceNow: Instance is unavailable.');
     }
 
     /**
@@ -159,7 +120,7 @@ class ServiceNowAdapter extends EventEmitter {
      */
     emitOnline() {
         this.emitStatus('ONLINE');
-        log.info('ServiceNow: Instance is available. | Instance id: ${this.id}');
+        log.info('ServiceNow: Instance is available.');
     }
 
     /**
@@ -191,10 +152,10 @@ class ServiceNowAdapter extends EventEmitter {
          * Note how the object was instantiated in the constructor().
          * get() takes a callback function.
          */
-        this.connector.get(data, error) => {
+        this.connector.get(data, error);
         this.process(data, error);
         callback(data, error);
-        };
+
     }
 
     /**
